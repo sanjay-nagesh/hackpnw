b8a363202c5a6aaf2fa78d76be1e5a064ca494d3
Sanjay Nagesh <sanjaynagesh55@outlook.com>
2023-02-05 13:46:15 -0800

    added changes

--- serverside.js
+++ serverside.js
@@ -1,45 +1,24 @@
 const express = require("express");
 const twilio = require("twilio");
-const { default: next } = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const app = express();
 
 const accountSid = 'your-twilio-account-sid';
 const authToken = 'your-twilio-auth-token';
 const client = new twilio(accountSid, authToken);
 
-app.prepare().then(() => {
-  const server = express();
+app.use(express.json()); 
+ 
+app.post("/send-message", (req, res) => {
+  const { phone_number, message } = req.body;
 
-  server.use(express.json());
-
-  server.post("/send-message", (req, res) => {
-    const { phone_number, message } = req.body;
-
-    client.messages
-      .create({
-        body: message,
-        from: 'your-twilio-phone-number',
-        to: phone_number
-      })
-      .then(message => {
+  client.messages
+    .create({
+      body: message,
+      from: 'your-twilio-phone-number',
+      to: phone_numbers
+    })
+    .then(message => {
         console.log(message.sid);
-        res.send(message);
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).send(err);
       });
-  });
-
-  server.get("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(3000, err => {
-    if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
-  });
-});
+  7});
